01e9f091232c95827f0eee3022cfa6963fcf7720
Gaoyagi <aoyagigeorge@gmail.com>
2020-06-10 21:26:54 -0700

    finished tutorial

--- app.js
+++ app.js
@@ -71,8 +71,10 @@ app.get('/events/new', (req, res) => {
 
 //View an Event
 app.get('/events/:id', (req, res) => {
-  console.log("here")
   models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then(event => {
+      let createdAt = event.createdAt;
+      createdAt = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
+      event.createdAtFormatted = createdAt;
       res.render('events-show', { event: event });
   }).catch((err) => {
       console.log(err.message);
